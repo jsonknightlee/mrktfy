2f41189a073c85c925bee7ee7ea26eae6723d246
jsonknightlee <jsonknightlee@gmail.com>
2025-08-15 20:35:43 +0100

    Branch viability check

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
+  const envFile =
+    process.env.EAS_BUILD_PROFILE === 'production'
+      ? '.env.production'
+      : '.env';
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       ['module:react-native-dotenv', {
         moduleName: '@env',
-        path: '.env',
-        allowUndefined: true
+        path: envFile,
+        allowUndefined: true,
       }],
-      ['react-native-worklets/plugin']
-    ]
+      ['react-native-worklets/plugin'], // keep last
+    ],
   };
 };
